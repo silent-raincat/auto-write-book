30dd364195d86c664c4fc70d92e2d39733c69ff5
silent-raincat <294798818@qq.com>
2026-02-25 21:47:12 +0800

    Fix: convert PORT to number for TypeScript

--- api/server.ts
+++ api/server.ts
@@ -16,7 +16,7 @@ try {
 /**
  * start server with port
  */
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 const HOST = process.env.HOST || '0.0.0.0';
 
 const server = app.listen(PORT, HOST, () => {
